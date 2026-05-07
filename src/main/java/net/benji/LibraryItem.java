a101b05646f72002986437d225a2aed60c8f5737
Benjamin Norbelie <Benjamin.norbelie@elev.ga.ntig.se>
2026-05-07 22:06:30 +0200

    Lagt till getters och setters för libaryitem

--- src/main/java/net/benji/LibraryItem.java
+++ src/main/java/net/benji/LibraryItem.java
@@ -5,11 +5,26 @@ public abstract class LibraryItem {
     private String title;
     private String id;
     private boolean isAvailable;
+
+    //constructor för att skapa en instans av LibraryItem
     public LibraryItem(String title, String id, boolean isAvailable) {
         this.title = title;
         this.id = id;
         this.isAvailable = isAvailable;
     }
 
-    
+    //Getters och setters för att komma åt och ändra hos LibraryItem.
+public String getTitle() {
+        return title;
+    }
+    public String getId() {
+        return id;
+    }
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
 }
