9aa3aec92a2de27d94dd8a501c2ededbf6282efa
Benjamin Norbelie <Benjamin.norbelie@elev.ga.ntig.se>
2026-05-08 14:27:06 +0200

    lagt till constructorer för libraryManager samt implementerat Gson

--- src/main/java/net/benji/LibraryManager.java
+++ src/main/java/net/benji/LibraryManager.java
@@ -2,12 +2,38 @@ package net.benji;
 
 import java.util.ArrayList;
 
+import com.google.gson.Gson;
+
 public class LibraryManager {
-        private ArrayList<LibraryItem> libraryItems; // Lista för alla biblioteksaker
+        //private ArrayList<LibraryItem> libraryItems; // Lista för alla biblioteksaker
         private ArrayList<String> Book; // Lista för böcker
         private ArrayList<String> Magazine; // Lista för magasin
-        //private ArrayList<suspendedUser> suspendedUsers; // Lista för avstängda användare
-        
 
+        
+        private Gson gson = new Gson(); // Skapa en instans av Gson för JSON-hantering
+
+//serever url hantering
+private String serverUrl;
+
+        public LibraryManager(String serverUrl) {
+                this.serverUrl = serverUrl; 
+        }
+        public void fetchBooks() {
+
+        }
+
+        public void fetchMagazines() {
+            
+        }
+
+        public void listLibraryItems() {
+            
+        }
+        public void addLibraryItem(LibraryItem item) {
+            
+        }
+        public void removeLibraryItem(LibraryItem item) {
+            
+        }
 
 }
