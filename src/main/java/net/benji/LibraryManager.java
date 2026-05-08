8c6f8269cdbcdc9a2e0ea3e6ac6d5e3912be77ad
Benjamin Norbelie <Benjamin.norbelie@elev.ga.ntig.se>
2026-05-08 15:02:12 +0200

    metod för hämta böcker tillagt samt unirest

--- src/main/java/net/benji/LibraryManager.java
+++ src/main/java/net/benji/LibraryManager.java
@@ -1,8 +1,16 @@
 package net.benji;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import net.benji.LibraryItems.Book;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import kong.unirest.core.HttpResponse;
+import kong.unirest.core.Unirest;
+import kong.unirest.core.UnirestException;
 
 public class LibraryManager {
         //private ArrayList<LibraryItem> libraryItems; // Lista för alla biblioteksaker
@@ -19,7 +27,18 @@ private String serverUrl;
                 this.serverUrl = serverUrl; 
         }
         public void fetchBooks() {
+            HttpResponse<String> response;
+            try {
+                response = Unirest.get(serverUrl + "/books").asString();
+                String jsonBody = response.getBody();
 
+                // Konvertera JSON-strängen till en lista av Book-objekt
+                List<Book> fetchedBooks = gson.fromJson(jsonBody, new TypeToken<List<Book>>(){}.getType());
+                
+
+            } catch (UnirestException e) {
+                IO.println("Något blev fel vid inläsning.");
+            }
         }
 
         public void fetchMagazines() {
