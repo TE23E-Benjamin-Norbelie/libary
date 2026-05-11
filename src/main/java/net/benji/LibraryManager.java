1ae1177839f4ca7e6a886d2919e48b69e42a0de3
Benjamin Norbelie <Benjamin.norbelie@elev.ga.ntig.se>
2026-05-11 12:50:09 +0200

    fixade funktioner för fetch book och magzine

--- src/main/java/net/benji/LibraryManager.java
+++ src/main/java/net/benji/LibraryManager.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import net.benji.LibraryItems.Book;
+import net.benji.LibraryItems.Magazine;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -14,8 +15,8 @@ import kong.unirest.core.UnirestException;
 
 public class LibraryManager {
         //private ArrayList<LibraryItem> libraryItems; // Lista för alla biblioteksaker
-        private ArrayList<String> Book; // Lista för böcker
-        private ArrayList<String> Magazine; // Lista för magasin
+        private ArrayList<Book> books; // Lista för böcker
+        private ArrayList<Magazine> magazines; // Lista för magasin
 
         
         private Gson gson = new Gson(); // Skapa en instans av Gson för JSON-hantering
@@ -34,6 +35,7 @@ private String serverUrl;
 
                 // Konvertera JSON-strängen till en lista av Book-objekt
                 List<Book> fetchedBooks = gson.fromJson(jsonBody, new TypeToken<List<Book>>(){}.getType());
+                books.addAll(fetchedBooks);
                 
 
             } catch (UnirestException e) {
@@ -42,7 +44,17 @@ private String serverUrl;
         }
 
         public void fetchMagazines() {
-            
+            HttpResponse<String> response;
+            try {
+                response = Unirest.get(serverUrl + "/magazines").asString();
+                String jsonBody = response.getBody();
+
+                // Konvertera JSON-strängen till en lista av Magazine-objekt
+                List<Magazine> fetchedMagazines = gson.fromJson(jsonBody, new TypeToken<List<Magazine>>(){}.getType());
+
+            } catch (UnirestException e) {
+                IO.println("Något blev fel vid inläsning.");
+            }
         }
 
         public void listLibraryItems() {
